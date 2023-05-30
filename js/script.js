264d84498dc4c0736e67975b2d1dfee3445162ca
deborah-andrade <deborahandraderibeiro44@gmail.com>
2023-05-30 14:30:40 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -121,6 +121,34 @@ async function exibirDetalheDoLivro(bookId){
 }
 
 const parametrosURL = new URLSearchParams(window.location.search);
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  const imgmodal= parametrosURL.get('imagem');
+  document.querySelector("#img-capa-modal").setAttribute('src',imgmodal);
+  document.querySelector("#tituloLivro").textContent = livro.titulo;
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 const livro ={
   id: parametrosURL.get('id'),
