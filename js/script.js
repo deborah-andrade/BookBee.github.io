8f906342eca8fdcc55288e505ea0fa9e69908d9b
deborah-andrade <deborahandraderibeiro44@gmail.com>
2023-05-31 21:20:35 -0300

    modal javascrip

--- js/script.js
+++ js/script.js
@@ -140,6 +140,40 @@ function abrirModal1() {
   document.querySelector("#titulo-modal-notas").textContent = livro.titulo;
 }
 
+// Função para fechar o modal 1
+function fecharModal1() {
+  modal1.style.display = "none";
+}
+
+// Função para abrir o modal 2
+function abrirModal2() {
+  modal2.style.display = "block";
+  const imgmodal = parametrosURL.get('imagem');
+  document.querySelector("#img-capa-modal").setAttribute('src', imgmodal);
+  document.querySelector("#titulo-modal").textContent = livro.titulo;
+}
+
+// Função para fechar o modal 2
+function fecharModal2() {
+  modal2.style.display = "none";
+}
+
+// Atribuindo as funções aos botões correspondentes
+btn1.onclick = abrirModal1;
+btn2.onclick = abrirModal2;
+
+// Atribuindo as funções de fechar os modais aos botões de fechamento correspondentes
+span1.onclick = fecharModal1;
+span2.onclick = fecharModal2;
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    fecharModal1();
+  }
+  if (event.target == modal2) {
+    fecharModal2();
+  }
+}
 
 const livro ={
   id: parametrosURL.get('id'),
