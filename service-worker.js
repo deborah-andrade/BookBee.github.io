0771fa7ab35d2dfb15ea80c7952c08988dd77461
ly1359 <diellylopes330@gmail.com>
2023-06-02 11:57:01 -0300

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 self.addEventListener('install', async event => {
     const cache = await caches.open('my-cache');
     try{
-        await cache.add('/inicial.html');
-        await cache.add( '/css/style.css');
-        await cache.add('/js/script.js');
+        await cache.add('inicial.html');
+        await cache.add( 'css/style.css');
+        await cache.add('js/script.js');
         console.log('arquivo adicionados ao cache')
     } catch (error) {
         console.log("falha ao adicionar ao cache:", error);
