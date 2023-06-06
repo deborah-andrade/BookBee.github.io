d0f537dcf08ca3dd888ccd715b1a82d6f15b82b0
ly1359 <diellylopes330@gmail.com>
2023-06-06 12:11:40 -0300

    add - todos os favoritos

--- js/busca.js
+++ js/busca.js
@@ -1,31 +1,3 @@
-function mobileMenu() {
-  const mobileMenu = document.querySelector(".mobile-menu");
-  const menuList = document.querySelector(".menu ul");
-  const menuLinks = document.querySelectorAll(".menu li");
-  const activeClass = "active";
-
-  if (mobileMenu) {
-      handleClick();
-  } else {
-      return 0;
-  }
-
-  function handleClick() {
-      menuList.classList.toggle(activeClass);
-      mobileMenu.classList.toggle(activeClass);
-      animateLinks();
-  }
-
-  function animateLinks() {
-      menuLinks.forEach((link, index) => {
-          link.style.animation
-              ? (link.style.animation = "")
-              : (link.style.animation = `menuLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-      });
-  }
-}
-
-
 const searchIcon = document.querySelector('.barraDeBusca .iconeBusca');
 const searchInput = document.querySelector('.barraDeBusca input');
 const searchButton = document.querySelector('.barraDeBusca button');
--- js/cronometro.js
+++ js/cronometro.js
@@ -42,7 +42,7 @@ function paraCronometro () {
 };
     
 function zerarCronometro(){
-    // clearInterval(cronometro);
+    clearInterval(cronometro);
     tempoMilesimos = 0;
     tempoSegundos = 0;
     tempoMinutos = 0;
@@ -53,5 +53,84 @@ function zerarCronometro(){
     document.querySelector('.tempo.Horas').textContent = '00';
 }
 
-  
+const playBtn = document.querySelector(".play");
+const lapBtn = document.querySelector(".lap");
+const resetBtn = document.querySelector(".reset");
+const minute = document.querySelector(".min");
+const second = document.querySelector(".sec");
+const centiSecond = document.querySelector(".msec");
+const laps = document.querySelector(".laps");
+const clearAllBtn = document.querySelector(".lap-clear-btn");
+const bg = document.querySelector(".outer-circle");
+let isplay = false;
+let minCounter = 0;
+let min;
+let secCounter = 0;
+let sec;
+let centiCounter =0;
+let centiSec;
+let lapId = 0;
+let timerInterval;
+
+function startTimer(){
+    if(!isplay){
+        isplay = true;
+        playBtn.textContent = "Pause";
+        timerInterval = setInterval(updateTimer, 10);
+    }else{
+        isplay = false;
+        playBtn.textContent = "Resume";
+        clearInterval(timerInterval);
+    }
+}
+
+function updateTimer() {
+    centiCounter++;
+    if(centiCounter === 100){
+        centiCounter= 0;
+        secCounter++;
+    }
+    if(secCounter === 60){
+        secCounter = 0;
+        minCounter++;
+    }
+    min = minCounter.toString().padStart(2, "0");
+    sec = secCounter.toString().padStart(2, "0");
+    centiSec = centiCounter.toString().padStart(2, "0");
+    minute.textContent = min + " :";
+    second.textContent = " " + sec + " :";
+    centiSecond.textContent = " " + centiSec;
+}
+
+function resetTimer(){
+    isplay = false;
+    clearInterval(timerInterval);
+    minCounter = 0;
+    secCounter = 0;
+    centiCounter = 0;
+    minute.textContent = min + " :";
+    second.textContent = " " + sec + " :";
+    centiSecond.textContent = " " + centiSec;
+    playBtn.textContent = "Play";
+    laps.innerHTML = "";
+    lapId = 0;
+}
+
+function lapTimer(){
+    if(isplay){
+        const lapItem = document.createElement("li");
+        lapItem.textContent = `Lap ${++lapId}: ${min}:${sec}.${centiSec}`;
+        laps.appendChild(lapItem);
+    }
+}
+
+function clearAllLaps() {
+    laps.innerHTML = "";
+    lapId = 0;
+}
+
+playBtn.addEventListener('click', startTimer);
+resetBtn.addEventListener('click', resetTimer);
+lapBtn.addEventListener('click', lapTimer);
+clearAllBtn.addEventListener('click', clearAllLaps);
   
